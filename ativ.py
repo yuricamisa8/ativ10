ca76af9263bb758fa8bae43b2dededc59a46ca6c
yuricamisa8 <francisco.cardoso94@aluno.ce.gov.br>
2024-09-11 14:26:48 -0300

    first commit

---
+++ ativ.py
@@ -0,0 +1,6 @@
+# Crie um programa que receba um número inteiro e informe se ele é par ou ímpar.
+n = int(input("crie um programa que dega se e impar ou par"))
+if n % 2: print("impar")
+else: 
+ print('par')
+    
